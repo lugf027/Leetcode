2dafb27a04addff6fb71f8fdfdd24dd3fc564240
lugf027 <161230578@qq.com>
2020-04-14 23:59:00 +0800

    添加题目

--- src/April2020/convert/Solution.java
+++ src/April2020/convert/Solution.java
@@ -1,5 +1,15 @@
 package April2020.convert;
 
+/**
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ *
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ */
 public class Solution {
     /*
     执行用时 :3 ms, 在所有 Java 提交中击败了99.57%的用户
